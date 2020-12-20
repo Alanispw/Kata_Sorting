23a7768caeaa1338f36fd918e2a73cb27192cdb2
Alanis Pereyra <alaniswagner14@hotmail.com>
2020-12-19 16:11:28 -0800

    Test case 2 passed

--- Sorting.Tests/Tests.fs
+++ Sorting.Tests/Tests.fs
@@ -9,3 +9,9 @@ let ``Test case 1 - [1;5;7;8;2;3] sorted is equal to [1;2;3;5;7;8]`` () =
     let actual = Main.quicksort [1;5;7;8;2;3] 
     let expected = [1;2;3;5;7;8]
     Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
+
+[<Fact>]
+let ``Test case 2 - [8;2;7;8;1;4] sorted is equal to [1;2;4;7;8;8]`` () =
+    let actual = Main.quicksort [8;2;7;8;1;4]
+    let expected = [1;2;4;7;8;8]
+    Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
